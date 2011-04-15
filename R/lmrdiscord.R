ecbd9971a6f6b9ed416b324aada5c8db8f1a4e7c
William H. Asquith <william.asquith@ttu.edu>
2011-04-15 00:00:00 +0000

    version 1.3.4

--- R/lmrdiscord.R
+++ R/lmrdiscord.R
@@ -1,6 +1,6 @@
 "lmrdiscord" <-
-function(group=NULL, t2=NULL, t3=NULL, t4=NULL,
-         Dcrit=NULL, digits=4, sort=TRUE,
+function(site=NULL, t2=NULL, t3=NULL, t4=NULL,
+         Dcrit=NULL, digits=4, lmrdigits=4, sort=TRUE,
          alpha1=0.10, alpha2=0.01) {
 
   if(alpha1 < alpha2) {
@@ -10,17 +10,17 @@ function(group=NULL, t2=NULL, t3=NULL, t4=NULL,
     alpha2 <- alpha
   }
 
-  if(is.null(group)) group <- 1:length(t2)
-  ngroup <- length(group)
+  if(is.null(site)) site <- 1:length(t2)
+  nsite <- length(site)
 
-  if(ngroup < 5) {
-     warning("Too few groups (<5), although >10 is preferred")
+  if(nsite < 5) {
+     warning("Too few sites (<5), although >10 is preferred")
      return(NULL)
   }
 
-  if(length(t2) != ngroup) stop("Tau2 length is inconsistent with group length")
-  if(length(t3) != ngroup) stop("Tau3 length is inconsistent with group length")
-  if(length(t4) != ngroup) stop("Tau4 length is inconsistent with group length")
+  if(length(t2) != nsite) stop("Tau2 length is inconsistent with site length")
+  if(length(t3) != nsite) stop("Tau3 length is inconsistent with site length")
+  if(length(t4) != nsite) stop("Tau4 length is inconsistent with site length")
 
   tabDc <- list(Dc = c(1.3333, 1.6481, 1.9166, 2.1401, 2.3287,
                        2.4906, 2.6321, 2.7573, 2.8694, 2.9709, 3),
@@ -37,7 +37,7 @@ function(group=NULL, t2=NULL, t3=NULL, t4=NULL,
   for(j in 1:3) {
     for(k in j:3) {
       SSMAT[j,k] <- 0
-      for(i in 1:ngroup) {
+      for(i in 1:nsite) {
         SSMAT[j,k] <- SSMAT[j,k] + tmp[i,j] * tmp[i,k]
       }
     }
@@ -45,35 +45,38 @@ function(group=NULL, t2=NULL, t3=NULL, t4=NULL,
   SSMAT <- chol2inv(chol(SSMAT)) # Matrix inversion
 
   # Hosking and Wallis (1997, eq. 3.3)
-  D <- rep(0, ngroup)
-  for(i in 1:ngroup) {
+  D <- rep(0, nsite)
+  for(i in 1:nsite) {
     for(j in 1:3) {
       for(k in 1:3) {
         D[i] <- D[i] + tmp[i,j] * tmp[i,k] * SSMAT[j,k]
       }
     }
   }
-  D <- ngroup*D/3
+  D <- nsite*D/3
 
   Dc <- Dcrit
   if(is.null(Dc)) {
-    Dc <- ifelse(ngroup >= 15, 3, tabDc$Dc[tabDc$n == ngroup])
+    Dc <- ifelse(nsite >= 15, 3, tabDc$Dc[tabDc$n == nsite])
   }
-  isD <- rep(FALSE, ngroup)
+  isD <- rep(FALSE, nsite)
   isD[D > Dc] <- TRUE
 
-  Z <- qf(alpha1/ngroup, 3, ngroup - 4, lower.tail=FALSE)
-  D1.by.Fdist <- (ngroup - 1)*Z/(ngroup - 4 + 3*Z)
+  Z <- qf(alpha1/nsite, 3, nsite - 4, lower.tail=FALSE)
+  D1.by.Fdist <- (nsite - 1)*Z/(nsite - 4 + 3*Z)
 
-  Z <- qf(alpha2/ngroup, 3, ngroup - 4, lower.tail=FALSE)
-  D2.by.Fdist <- (ngroup - 1)*Z/(ngroup - 4 + 3*Z)
+  Z <- qf(alpha2/nsite, 3, nsite - 4, lower.tail=FALSE)
+  D2.by.Fdist <- (nsite - 1)*Z/(nsite - 4 + 3*Z)
 
-  star <- rep("-", ngroup)
+  star <- rep("-", nsite)
   star[D > D1.by.Fdist] <- "*"
   star[D > D2.by.Fdist] <- "**"
 
-  z <- data.frame(group=group, t2=t2, t3=t3, t4=t4,
-                  Dmax=round((ngroup - 1)/3, digits=digits),
+  z <- data.frame(site=site,
+                  t2=round(t2, digits=lmrdigits),
+                  t3=round(t3, digits=lmrdigits),
+                  t4=round(t4, digits=lmrdigits),
+                  Dmax=round((nsite - 1)/3, digits=digits),
                   Dalpha1=round(D1.by.Fdist, digits=digits),
                   Dalpha2=round(D2.by.Fdist, digits=digits),
                   Dcrit=Dc, D=round(D, digits=digits), isD=isD,
